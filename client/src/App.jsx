19eeccf374078d1145483a4b735992444f347514
= <=>
2023-10-08 23:03:44 -0400

    Server code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   
 const[email, setEmail] = useState('');
 const[password, setPassword] = useState('');
+const handleSubmit =(e)=>{
+  e.preventDefault();
+  axios.post('http://localhost:3001/login', {email,password})
+  .then(result => console.log(result))
+  .catch(err => console.log(err))
+}
   return (
     <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
       <div className='bg-white p-3 rounded w-25'>
         <h2>Sign-In</h2>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className='mb-3'>
             <label htmlFor='email'><strong>Email</strong></label>
             <input type='email' placeholder='Enter Email' name='email' className='form-control rounded-0'
---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app= express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://chandoksahej:3iSwiEatn0Z9BBB5@cluster0.rktwo6l.mongodb.net/?retryWrites=true&w=majority')
+
+const UserSchema = new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+const UserModel = mongoose.model("users", UserSchema)
+
+app.post("/login", (req,res) =>{
+    const {email, password} =req.body;
+    UserModel.findOne({email: email})
+    .then(user => {
+        if(user) {
+            if(user.password == password){
+                res.json("Login Successful")
+            } else{
+                res.json("The password is incorrect")
+            }
+        }else{
+            res.json("no record existed")
+        }
+    })
+})
+app.listen(3001, ()=> {
+    console.log("Server is running")
+})
